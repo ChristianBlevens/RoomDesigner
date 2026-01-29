8906299774b5717de7a3d930943cce7a4bc92081
ChristianBlevens <138273879+ChristianBlevens@users.noreply.github.com>
2026-01-28 23:54:48 -0800

    Update lbm_endpoint.py

--- modal/lbm_endpoint.py
+++ modal/lbm_endpoint.py
@@ -40,8 +40,8 @@ image = (
     )
     .env({"HF_HOME": "/root/.cache/huggingface"})
     .pip_install(
-        "torch==2.5.1",
-        "torchvision==0.20.1",
+        "torch==2.7.0",
+        "torchvision",
         "pillow",
         "numpy<2.0",
         "fastapi[standard]",
